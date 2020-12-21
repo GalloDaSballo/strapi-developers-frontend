e28f9c1cf1323e3d4a5e4b9064da30feae2a4d82
Entreprenerd <valeranijr@gmail.com>
2020-12-21 12:12:46 +0100

    feat ga and fb /Credits to NextJS Repo

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,44 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import { initGA, logPageView } from '../utils/ga'
+import * as fbq from '../utils/fb'
+
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+/**
+ * All tracking stuff on page change
+ */
+const handleRouteChange = () => {
+  fbq.pageview()
+  logPageView()
+}
+
+function MyApp ({ Component, pageProps }) {
+  const router = useRouter()
+
+  /** Credits: https://github.com/vercel/next.js/blob/canary/examples/with-react-ga/utils/analytics.js */
+  /** Also credits: https://github.com/vercel/next.js/blob/canary/examples/with-facebook-pixel/components/FacebookPixel.js */
+  useEffect(() => {
+    initGA()
+    // `routeChangeComplete` won't run for the first page load unless the query string is
+    // hydrated later on, so here we log a page view if this is the first render and
+    // there's no query string
+    if (!router.asPath.includes('?')) {
+      logPageView()
+    }
+    //First Pixel Ping
+    fbq.pageview()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  useEffect(() => {
+    // Listen for page changes after a navigation or when the query changes
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return <Component {...pageProps} />
 }
 
