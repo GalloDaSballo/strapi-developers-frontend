be3efa6be72b670dff4e6de8bd938aba5fbb2d33
Entreprenerd <valeranijr@gmail.com>
2021-01-01 15:23:08 +0100

    Tag filtering actually works

--- components/Tags.js
+++ components/Tags.js
@@ -1,36 +1,15 @@
 import styles from '../styles/Tags.module.css';
-import { useState } from 'react';
 import Tag from '../components/Tag';
 
-export default function Tags({ tags }) {
-    const [activeTags, setActiveTags] = useState(['all tags']);
-
+export default function Tags({ tags, activeTags, toggleTag }) {
     /**
      * Given a tag return whether the tag is active or not
      * @param {any} tag
      */
     const isTagActive = (tag) => {
-        console.log('activeTags', activeTags);
         let t = activeTags.find((item) => item === tag.label);
         if (t) return true;
         else return false;
-    };
-
-    /**
-     * Given a tag activates it if it was inactive, deactiveded if it was active
-     * @param {any} tag
-     */
-    const toggleTag = (tag) => {
-        let found = activeTags.find((item) => item === tag.label);
-        if (found) {
-            let newTags = [...activeTags]; // Copia
-            newTags = newTags.filter((item) => item !== tag.label);
-            setActiveTags(newTags);
-        } else {
-            const newTags = [...activeTags]; // Copia
-            newTags.push(tag.label);
-            setActiveTags(newTags);
-        }
     };
 
     return (
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { useState, useEffect, useMemo } from 'react';
 import Head from 'next/head';
 import Masthead from '../components/Masthead';
 import Profiles from '../components/Profiles';
@@ -5,15 +6,62 @@ import Tags from '../components/Tags';
 
 import { API_URL } from '../utils/urls';
 
+const useActiveTagsAndProfiles = (profiles, tags) => {
+    const [activeTags, setActiveTags] = useState([]);
+
+    useEffect(() => {
+        setActiveTags([]);
+    }, [tags]);
+
+    /**
+     * Given a tag activates it if it was inactive, deactiveded if it was active
+     * @param {any} tag
+     */
+    const toggleTag = (tag) => {
+        let found = activeTags.find((item) => item === tag.label);
+        if (found) {
+            let newTags = [...activeTags]; // Copia
+            newTags = newTags.filter((item) => item !== tag.label);
+            setActiveTags(newTags);
+        } else {
+            const newTags = [...activeTags]; // Copia
+            newTags.push(tag.label);
+            setActiveTags(newTags);
+        }
+    };
+
+    const activeProfiles = useMemo(() => {
+        try {
+            if (activeTags.length) {
+                console.log('activeTags', activeTags);
+                return profiles.filter(
+                    (profile) =>
+                        profile.skills && profile.skills.some((r) => activeTags.includes(r.label))
+                );
+            }
+        } catch (err) {
+            console.log('Exception in tag filtering');
+        }
+
+        return profiles;
+    }, [activeTags]);
+
+    console.log('activeProfiles', activeProfiles);
+
+    return { activeTags, toggleTag, activeProfiles };
+};
+
 export default function Home({ profiles, tags }) {
+    const { activeTags, toggleTag, activeProfiles } = useActiveTagsAndProfiles(profiles, tags);
+
     return (
         <>
             <Head>
                 <title>Strapi Developers</title>
             </Head>
             <Masthead />
-            <Tags tags={tags} />
-            <Profiles profiles={profiles} />
+            <Tags tags={tags} activeTags={activeTags} toggleTag={toggleTag} />
+            <Profiles profiles={activeProfiles} />
         </>
     );
 }
